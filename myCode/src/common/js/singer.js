e6cba40171aa8a4ebf8a644daa7715b0c5294895
cckings <1504468447@qq.com>
2018-04-10 11:00:31 +0800

    歌手列表搭建

---
+++ myCode/src/common/js/singer.js
@@ -0,0 +1,5 @@
+export default function Singer({name,id}) {
+  this.name = name;
+  this.id = id;
+  this.avtor = `https://y.gtimg.cn/music/photo_new/T001R150x150M000${id}.jpg?max_age=2592000`
+}
--- myCode/src/components/singer/singer.vue
+++ myCode/src/components/singer/singer.vue
@@ -3,7 +3,14 @@
     <div class="mainBox" ref="singerList">
       <ul class="content singerList">
         <li v-for="(item,index) in singerList" :key="index">
-          {{item.Fsinger_name}}
+          <dl>
+            <dt class="Dtitle everyList">
+              {{item.title}}
+            </dt>
+            <dd v-for="(ditem , dindex) in item.item" :key="dindex" class="ddItem everyList">
+              {{ditem.name}}
+            </dd>
+          </dl>
         </li>
       </ul>
     </div>
@@ -13,6 +20,7 @@
 <script>
   import {getSingerData} from '@/api/singer';
   import BScroll from 'better-scroll'
+  import Singer from '@/common/js/singer'
 export default {
   name: 'singer',
   data () {
@@ -25,13 +33,8 @@ export default {
     _singerData:function () {
       var that = this;
       getSingerData().then(function (res) {
-        console.log(res);
-        var singerList = []
-        res.data.list.forEach(function (item,index) {
-          singerList.push(item)
-        })
-        that.singerList = singerList;
-        /*that.singerList = singerData.sort((a, b) => a.Fsinger_name.localeCompare(b.Fsinger_name, 'zh-Hans-CN', {sensitivity: 'accent'}))*/
+        that.singerList = that._dataSort(res.data.list);
+        console.log(that.singerList);
       }).catch(function (res) {
         console.log(res);
       })
@@ -40,6 +43,51 @@ export default {
       this.singerListScroll = new BScroll(this.$refs.singerList, {
         click: true
       });
+    },
+    _dataSort: function (list) {
+      var newList = {
+        hot:{
+          title:'热门',
+          item:[]
+        }
+      };
+      list.forEach((item,index) => {
+        if(index < 10){
+          newList.hot.item.push(
+            new Singer({
+              name:item.Fsinger_name,
+              id:item.Fsinger_mid
+            })
+          )
+        }
+
+        if(!newList[item.Findex]){
+          newList[item.Findex] = {
+            title:item.Findex,
+            item:[]
+          }
+        }
+        newList[item.Findex].item.push(
+          new Singer({
+            name:item.Fsinger_name,
+            id:item.Fsinger_mid
+          })
+        )
+      })
+
+      var hot = [],ret = [];
+      for(let key in newList){
+        if(newList[key].title.match(/[a-zA-z]/)){
+          ret.push(newList[key])
+        }else if(newList[key].title == '热门'){
+          hot.push(newList[key])
+        }
+      }
+      ret.sort((a,b) => {
+        return a.title.charCodeAt(0) - b.title.charCodeAt(0)
+      })
+
+      return hot.concat(ret)
     }
   },
   created() {
@@ -57,9 +105,19 @@ export default {
   li{
     width: 100%;
     display: block;
-    padding-left: 30px;
     line-height: 60px;
     background-color: #ffffff;
+    .everyList{
+      height: 60px;
+      line-height: 60px;
+      padding-left: 30px;
+    }
+    .Dtitle{
+      background: #e6e6e6;
+    }
+    .ddItem{
+
+    }
   }
 }
 </style>
